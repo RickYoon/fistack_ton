69d5d55c3a22add5ab0a35f57449b02ae9ab98bf
ilo yoon <iloyoon@MacBook-Pro-7.local>
2024-06-16 17:02:48 +0900

    update manifest

--- src/index.js
+++ src/index.js
@@ -9,50 +9,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { TonConnectUIProvider  } from '@tonconnect/ui-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const manifestUrl = 'https://main.d56xyn8hin4yq.amplifyapp.com/tonconnect-manifest.json';
 
 root.render(
   <React.StrictMode>
   <TonConnectUIProvider
           manifestUrl="https://main.d56xyn8hin4yq.amplifyapp.com/tonconnect-manifest.json"
-          walletsListConfiguration={{
-            includeWallets: [
-              {
-                appName: "safepalwallet",
-                name: "SafePal",
-                imageUrl: "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
-                tondns: "",
-                aboutUrl: "https://www.safepal.com",
-                universalLink: "https://link.safepal.io/ton-connect",
-                jsBridgeKey: "safepalwallet",
-                bridgeUrl: "https://ton-bridge.safepal.com/tonbridge/v1/bridge",
-                platforms: ["ios", "android", "chrome", "firefox"]
-              },
-              {
-                appName: "bitgetTonWallet",
-                name: "Bitget Wallet",
-                imageUrl: "https://raw.githubusercontent.com/bitkeepwallet/download/main/logo/png/bitget%20wallet_logo_iOS.png",
-                aboutUrl: "https://web3.bitget.com",
-                deepLink: "bitkeep://",
-                jsBridgeKey: "bitgetTonWallet",
-                bridgeUrl: "https://bridge.tonapi.io/bridge",
-                platforms: ["ios", "android", "chrome"],
-                universalLink: "https://bkcode.vip/ton-connect"
-              },
-              {
-                appName: "tonwallet",
-                name: "TON Wallet",
-                imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
-                aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
-                universalLink: "https://wallet.ton.org/ton-connect",
-                jsBridgeKey: "tonwallet",
-                bridgeUrl: "https://bridge.tonapi.io/bridge",
-                platforms: ["chrome", "android"]
-              }
-            ]
-          }}
           actionsConfiguration={{
-              twaReturnUrl: 'https://t.me/DemoDappWithTonConnectBot/demo'
+              twaReturnUrl: 'https://t.me/hellotma_bot'
           }}
       >
             <Provider store={store}>
